875dcd43d702e2d68ceac638cd21d2f9c098b708
adamovd <adam.ov.danielsson@gmail.com>
2023-03-13 10:23:25 +0100

    added questions

---
+++ src/ts/models/questions.ts
@@ -0,0 +1,52 @@
+export const questions = [
+  {
+    lyric: "I came in like a wrecking ball",
+    options: ["Taylor Swift", "Ariana Grande", "Miley Cyrus"],
+    answer: "Miley Cyrus",
+  },
+  {
+    lyric: "Cause, baby, you're a firework",
+    options: ["Katy Perry", "Rihanna", "Lady Gaga"],
+    answer: "Katy Perry",
+  },
+  {
+    lyric: "I'm friends with the monster that's under my bed",
+    options: ["Eminem", "Kendrick Lamar", "Jay-Z"],
+    answer: "Eminem",
+  },
+  {
+    lyric: "I don't want a lot for Christmas",
+    options: ["Mariah Carey", "Whitney Houston", "Celine Dion"],
+    answer: "Mariah Carey",
+  },
+  {
+    lyric: "I'm a survivor, I'm not gonna give up",
+    options: ["Beyoncé", "Alicia Keys", "Christina Aguilera"],
+    answer: "Beyoncé",
+  },
+  {
+    lyric: "I see a little silhouetto of a man",
+    options: ["Queen", "The Beatles", "Led Zeppelin"],
+    answer: "Queen",
+  },
+  {
+    lyric: "We're no strangers to love",
+    options: ["Michael Jackson", "Rick Astley", "George Michael"],
+    answer: "Rick Astley",
+  },
+  {
+    lyric: "I got my head checked by a jumbo jet",
+    options: ["Blur", "Oasis", "Radiohead"],
+    answer: "Blur",
+  },
+  {
+    lyric: "The sun'll come out tomorrow",
+    options: ["Annie", "Les Misérables", "The Sound of Music"],
+    answer: "Annie",
+  },
+  {
+    lyric: "I believe I can fly",
+    options: ["Usher", "R. Kelly", "Chris Brown"],
+    answer: "R. Kelly",
+  },
+];
