529aa92288e6b7cc711bdff4f3b7c36cdba907a7
adamovd <adam.ov.danielsson@gmail.com>
2023-03-13 12:42:30 +0100

    merge highscore to main

--- src/ts/models/highscores.ts
+++ src/ts/models/highscores.ts
@@ -3,5 +3,6 @@ import { Highscore } from "./Highscore";
 export const highscores: Highscore[] = [
   new Highscore("Adam", 10),
   new Highscore("Wilma", 8),
+  new Highscore("Corny Peanut", 6),
   new Highscore("Felicia", 0),
 ];
